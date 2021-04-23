86502f0eb3e3128a57f4d4dbdcfc8be78eb00010
mengtianao <t5meng@uwaterloo.ca>
2021-04-23 13:19:17 -0400

    First commit

---
+++ src/main/java/com/example/springbootbackend/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package com.example.springbootbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.example.springbootbackend.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	
+
+}
+
